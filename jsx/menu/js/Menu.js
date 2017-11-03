9398781d06548fee33f39dfd26ccf973d8182d3c
petrelevich <petrelevich@yandex.ru>
2017-11-03 20:47:28 +0300

    Homework 1, fixed review comments

--- jsx/menu/js/Menu.js
+++ jsx/menu/js/Menu.js
@@ -1,22 +1,17 @@
 function Menu({items,opened}) {
 
-  if (opened === true) {
-    const itemsForUl = items.map((item, index) => <li key={index}><a href={item.href}>{item.title}</a></li>);
-    return (
-      <div className="menu menu-open">
-        <div className="menu-toggle"><span></span></div>
-        <nav>
-          <ul>
-              {itemsForUl}
-          </ul>
-        </nav>
-      </div>
-    );
-  }
-
+  const itemsForUl = items.map((item, index) => <li key={index}><a href={item.href}>{item.title}</a></li>);
   return (
-    <div className="menu">
+    <div className={opened ? 'menu menu-open' : 'menu'}>
       <div className="menu-toggle"><span></span></div>
+        {opened && (
+        <nav>
+            <ul>
+              {itemsForUl}
+            </ul>
+          </nav>
+        )}
     </div>
   );
 }
+
